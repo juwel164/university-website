0f9a89033cfa806aabb85c4ea425e8de397280ff
juwel164 <juwelhossain16457@gmail.com>
2023-08-02 22:24:46 +0600

    all

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'dhalijuwel1657@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply_to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html")
+?>
